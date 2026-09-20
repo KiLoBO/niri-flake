b438df75aa012c47960ad58610087462188fbc92
David Lewis <dclewiss17@gmail.com>
2026-09-19 19:16:16 -0700

    update lockfile

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nix-config.url = "github:KiLoBO/nix-config";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -28,7 +27,6 @@
       self,
       nix-config,
       nixpkgs,
-      nixpkgs-unstable,
       home-manager,
       catppuccin,
       stylix,
--- system-niri.nix
+++ system-niri.nix
@@ -3,7 +3,6 @@
   lib,
   pkgs,
   inputs,
-  pkgs-unstable,
   ...
 }:
 {
@@ -27,26 +26,20 @@
     "qtwebengine-5.15.19"
   ];
 
-  environment.systemPackages =
-    with pkgs;
-    [
-      libnotify
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-      xwayland-satellite
-      libsecret
-      # niri-stable
-      manix
+  environment.systemPackages = with pkgs; [
+    libnotify
+    xdg-desktop-portal-gnome
+    xdg-desktop-portal-gtk
+    xwayland-satellite
+    libsecret
+    # niri-stable
+    manix
 
-      qt6Packages.qt5compat
-      libsForQt5.qt5.qtgraphicaleffects
-      kdePackages.qtbase
-      kdePackages.qtdeclarative
-      kdePackages.qtstyleplugin-kvantum
-      matugen
-    ]
-    ++ (with pkgs-unstable; [
-      # nmgui
-      rio
-    ]);
+    qt6Packages.qt5compat
+    libsForQt5.qt5.qtgraphicaleffects
+    kdePackages.qtbase
+    kdePackages.qtdeclarative
+    kdePackages.qtstyleplugin-kvantum
+    matugen
+  ];
 }
